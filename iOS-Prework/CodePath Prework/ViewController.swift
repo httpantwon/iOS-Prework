af7f6629187eb071b2f7e93e79afe0b9425e38ee
httpantwon <67429225+httpantwon@users.noreply.github.com>
2024-02-05 18:58:52 -0600

    Add files via upload

--- iOS-Prework/CodePath Prework/ViewController.swift
+++ iOS-Prework/CodePath Prework/ViewController.swift
@@ -8,27 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var changeBackgroundColor: UIButton!
-   
     
-    func changeColor() -> UIColor {
-        
-        
-        let red = CGFloat.random(in: 0...1)
-        let green = CGFloat.random(in: 0...1)
-        let blue = CGFloat.random(in: 0...1)
-        
-        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func changeBackgroundClor(_ sender: UIButton) {
         let randomColor = changeColor()
         view.backgroundColor = randomColor
     }
 
+    func changeColor() -> UIColor {
+        
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
 
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+        
+    }
 }
 
